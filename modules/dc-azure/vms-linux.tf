9f65f99a9a60bca5b6c036200047585f2b2694b2
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-04-14 15:44:02 +0200

    feaat: modules/dc-azure: use cloud-init/bootstrap for linux servers

--- modules/dc-azure/vms-linux.tf
+++ modules/dc-azure/vms-linux.tf
@@ -1,3 +1,14 @@
+module "linux_bs" {
+  source = "../linux-as-server"
+  for_each = var.vms_linux
+
+  gzip          = true
+  base64_encode = true
+
+  params = try(each.value.params, {})
+  type   = try(each.value.type, null)
+}
+
 module "vm_linux" {
   source   = "github.com/rweglarz/c-azure//modules/linux"
   for_each = var.vms_linux
@@ -10,4 +21,5 @@ module "vm_linux" {
   public_key          = var.ssh_public_key
   username            = each.value.auth.username
   password            = each.value.auth.password
+  custom_data         = try(module.linux_bs[each.key].rendered, null)
 }
