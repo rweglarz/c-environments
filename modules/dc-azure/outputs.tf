d8e1d4df91b8baac9b6f438ef81dc436631ddc8d
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-04-14 19:17:24 +0200

    feat: modules/dc-azure: output vnet ids

--- modules/dc-azure/outputs.tf
+++ modules/dc-azure/outputs.tf
@@ -5,3 +5,11 @@ output "vms_linux" {
 output "vms_ztna" {
   value = { for k,v in module.vm_ztna: k => v.private_ip_address }
 }
+
+output "vnet_ids" {
+  value = {
+    sec         = module.vnet_sec.vnet.id
+    workloads-a = module.vnet_workloads_a.vnet.id
+    workloads-b = module.vnet_workloads_b.vnet.id
+  }
+}
