f077600dc65b6bf4356f5e05f28b6007e213dd2d
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-04-17 12:52:13 +0200

    chore: eccbf694cda70: rename file and disable scm rules

---
+++ eccbf694cda70/scm.tf
@@ -0,0 +1,17 @@
+# resource "scm_security_rule" "rule1" {
+#   name         = "${local.env_prefix}-rule1"
+#   folder       = local.pa_folder
+#   action       = "deny"
+#   source_users = ["any"]
+#   sources = [
+#     "192.168.66.1"
+#   ]
+#   destinations = [
+#     "192.168.66.255"
+#   ]
+#   applications = ["any"]
+#   categories   = ["any"]
+#   services     = ["any"]
+#   froms        = ["any"]
+#   tos          = ["any"]
+# }
