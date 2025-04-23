c96cfb1d69b8b4bd05d1e3e8ed14beac6442f5c3
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-04-23 10:36:08 +0200

    fix: modules/dc-gcp: add workload type to linux vms

--- modules/dc-gcp/outputs.tf
+++ modules/dc-gcp/outputs.tf
@@ -1,3 +1,3 @@
 output "vms_linux" {
-  value = { for k,v in google_compute_instance.vm_linux: k => v.network_interface[0].network_ip }
+  value = { for k,v in google_compute_instance.vm_linux: k => { private_ip_address = v.network_interface[0].network_ip, type = try(var.vms_linux[k].type, null) } }
 }
