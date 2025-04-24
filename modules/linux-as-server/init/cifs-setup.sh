70bebabf1756b40d10cdeb5c803842da6958da81
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-04-24 09:41:10 +0200

    feat: modules/linux-as-sever: fix unify paths on all file services

--- modules/linux-as-server/init/cifs-setup.sh
+++ modules/linux-as-server/init/cifs-setup.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Nothing to do"
-mkdir /data
-chown smb /data
-chmod 777 /data
-mkdir "/data/_hostname ${hostname}"
+mkdir -p /data/data
+chown smb:smb /data/data
+chmod 777 /data/data
+mkdir "/data/data/_hostname ${hostname}"
 (echo smb; echo smb) | smbpasswd -s -a smb
 
-wget https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.83-installer.msi -o /data/putty-installer.msi
+wget https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.83-installer.msi -o /data/data/putty-installer.msi
 
 cat <<EOF >> /etc/ssh/sshd_config.d/91-smb.conf
 Match User smb
   ChrootDirectory /data
-  ForceCommand internal-sftp
+  ForceCommand internal-sftp -d /data
   AllowTcpForwarding no
   X11Forwarding no
-  PasswordAuthentication no
+  PasswordAuthentication yes
 EOF
 
 /usr/bin/systemctl restart ssh
